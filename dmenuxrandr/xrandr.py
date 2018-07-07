a30c52566e71832d1c4c0396194c62913327a4df
Clement Hannicq <clementh@theodo.fr>
2018-07-07 10:25:38 +0200

    Document the xrandr utilities

--- dmenuxrandr/xrandr.py
+++ dmenuxrandr/xrandr.py
@@ -6,6 +6,15 @@ modeLine = re.compile(r'^ +([^ ]+)')
 
 
 def get_xrandr():
+    """
+    Gets the current monitors status as a dict { [monitorName]: { name, isMapped, isConnected, isPrimary, modes } where:
+        name is the output name (example: DP1) as a string
+        isMapped is a boolean describing whether the output is configured to display something
+        isConnected is a boolean describing whether there is a monitor connected to that output
+        isPrimary is a boolean set to True if that output is the primary output
+        modes is an array of possible output modes as strings (in the "1234x456" format
+    @return:
+    """
     outputs = {}
 
     raw_xrandr = check_output(['xrandr'])
@@ -30,16 +39,35 @@ def get_xrandr():
     return outputs
 
 
-def set_mode(output, mode):
+def set_mode(output: str, mode: str):
+    """
+    Changes the mode for the specified output
+    @param output: The output name
+    @param mode: The mode to switch to (example: "1920w1080")
+    @return: Nothing
+    """
     call(['xrandr', '--output', output, '--mode', mode])
 
 
-def set_position(output, position, reference, auto_mode=False):
+def set_position(output: str, position: str, reference: str, auto_mode=False):
+    """
+    Moves the output relatively to another
+    @param output: The output to move
+    @param position: The position to move it to (example "--above"):
+    @param reference: The output to use as a reference
+    @param auto_mode: Whether to add a --auto flag, usefull for unMapped outputs, defaults to False
+    @return: Nothing
+    """
     command = ['xrandr', '--output', output, position, reference]
     if auto_mode:
         command.append('--auto')
     call(command)
 
 
-def set_off(output):
+def set_off(output: str):
+    """
+    Turns off an output (effectively unMapping it)
+    @param output: The output to deactivate
+    @return: Nothing
+    """
     call(['xrandr', '--output', output, '--off'])
